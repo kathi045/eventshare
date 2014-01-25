0f12d45dffdf2dce25e3ba0ab5c74f0c6e803887
romanha <Roman.Habitzl@gmx.net>
2014-01-25 04:13:45 +0100

    Verbesserte Anzeige der Social Medias
    ausklappbare Social Media Informationen
    Facebook API Event-Titelbild anzeigen

--- pages/eventdetail/eventdetailview.php
+++ pages/eventdetail/eventdetailview.php
@@ -23,7 +23,18 @@ class Eventdetailview {
         $flickrtag = $event[0]["flickrtag"];
         $flickrembed = $event[0]["flickrembed"];
         
-        $out = "<div class='event'>
+        //JavaScript Funktion zum Toggeln der Visibility
+        $out = '<script type="text/javascript">
+            function toggle_visibility(id) {
+                var e = document.getElementById(id);
+                if(e.style.display == ' . "'block'" . ')
+                    e.style.display = ' . "'none'" . ';
+                else
+                    e.style.display = ' . "'block'" . ';
+            }
+        </script>';
+        
+        $out .= "<div class='event'>
                 <div class='event_rechts'>
                     <a class='button' href='?url=editevent&id=".$id."'><img class='editicon_mini' src='img/edit.png'>Event bearbeiten</a>
                 </div>
@@ -44,91 +55,7 @@ class Eventdetailview {
             $out .= "<h2>Zus&auml;tzliche Infos</h2>" . nl2br($addinfos) . "<br><br>";   // nl2b: new line to break (Zeilenumbrueche)
         }
         
-        
-        //Facebook Event
-        if ($fb_event_id) {
-            
-            $app_id = "557224121020260";
-            $app_secret = "f76394118a2abebaf00e19ce3215bc0a";
-            
-            $fb_config = array(
-                'appId' => $app_id,
-                'secret' => $app_secret,
-                'fileUpload' => false, // optional
-                'allowSignedRequest' => false, // optional, but should be set to false for non-canvas apps
-            );
-          
-            $facebook = new Facebook($fb_config);
-   
-            $out .= "<h2>Facebook Event:</h2>";
- 
-            $fb_link = "https://www.facebook.com/events/" . $fb_event_id . "/";            
-            $fb_name = $facebook->api("/" . $fb_event_id . "?fields=name");
-            $fb_name = $fb_name['name'];
-            
-            $out .= '<strong><a href="' . $fb_link . '">' . $fb_name . "</a></strong><br><br>";
-            
-            $attending = $facebook->api("/" . $fb_event_id . "/attending");
-            $attending = $attending['data'];
-            
-            $out .= count($attending) . " G&auml;ste haben zugesagt!<br><br>";
-            
-            foreach ($attending as $attendee) {
-                $out .= '<a href="https://www.facebook.com/' . $attendee['id'] . '">' . $attendee['name'] . "</a><br>";
-            }
-            
-            $out .= "<br><br>Neueste Kommentare:<br><br>";
-            
-            $feed = $facebook->api("/" . $fb_event_id . "/feed");
-            $feed = $feed['data'];
-            
-            foreach ($feed as $post) {
-                if ($post['type'] == "status") {
-                    $out .= '<a href="https://www.facebook.com/' . $post['from']['id'] . '">' . $post['from']['name'] . "</a><br>";
-                    $out .= $post['message'] . "<br><br>";
-                }
-                if ($post['story']) {
-                    $out .= '<a href="https://www.facebook.com/' . $post['from']['id'] . '">' . $post['from']['name'] . "</a><br>";                   
-                    $out .= $post['story'] . "<br>";
-                    if ($post['type'] == "photo") {
-                        $out .= '<img src="' . $post['picture'] . '" alt="Facebook Photo"><br>';
-                    }
-                    $out .= "<br>";
-                }
-            }           
-        }
-        
-        
-        //Twitter Hashtag --- reference: https://dev.twitter.com/docs/api/1.1/get/search/tweets, tutorial: http://www.youtube.com/watch?v=iPnGB7a7dO0
-        if($hashtag) {
-            
-            $consumer = "1stozpaVb8gfSdd15XI6xQ";
-            $consumersecret = "9scMIXAMJdZsPrTCeFyDPB35QU4aXcnkAoQ1u38Y";
-            $accesstoken = "2279704992-8Mf75D8VWn8VlRIc3oZbMnlyvR4c045Sl22r3am";
-            $accesstokensecret = "olZqD6MOdr86yv0qE56J5Q15QEQyB1mwnhX2cXtS8qzaZ";
-            
-            $out .= '<nr><br><br><img src="img/twitter_logo.png" alt="Twitter Bird" height="30px"><span class="hashtag">#' . $hashtag . '</span><br>';
-            
-            $twitter = new TwitterOAuth($consumer, $consumersecret, $accesstoken, $accesstokensecret);
-            $tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q=%23'.$hashtag.'&result_type=mixed');    //%23 wird als # aufgelöst (Hashtag)    //weitere Parameter: https://dev.twitter.com/docs/api/1.1/get/search/tweets
-            
-            $count = 0;
-            foreach($tweets as $tweet) {
-                foreach($tweet as $t) {
-                    $count++;
-                    if($count == 10) {
-                        break;
-                    }
-                    $out .= '<div class="tweets"><img src="'.$t->user->profile_image_url.'" />';
-                    $out .= '<span class="twittername">    ' . $t->user->name . '</span><br>';
-                    $out .= '<div class="twittertext">' . $t->text.'</div></div><br>';                                        //text gibt den Tweet aus
-                }
-                if($count == 10) {
-                    break;
-                }
-            }
-        }
-        
+
         /*
         // Google Maps x and y coords --- reference: http://w3schools.com/googleAPI/default.asp
         if($lat && $lng) {                
@@ -164,7 +91,8 @@ class Eventdetailview {
          *  Wandelt die Adresse des Event-Orts gleich in die Koordinaten um
          *  und erstellt dann eine Google Map mit dem Ort als Marker
          */
-        $out .= '<br><img src="img/google_maps_logo.png" width="200" alt="Google Maps"><br>
+        
+        $out .= '<br><br><img src="img/google_maps_logo.png" width="200" alt="Google Maps"><br>
                 <script>
                 var geocoder;
                 var map;
@@ -198,11 +126,125 @@ class Eventdetailview {
                 <div id="map-canvas"></div>
                 ';
         
+                
+        
+        
+        
+        //Facebook Event
+        //API Event Documentation:  https://developers.facebook.com/docs/graph-api/reference/event/
+        if ($fb_event_id) {
+            
+            $app_id = "557224121020260";
+            $app_secret = "f76394118a2abebaf00e19ce3215bc0a";
+            
+            $fb_config = array(
+                'appId' => $app_id,
+                'secret' => $app_secret,
+                'fileUpload' => false, // optional
+                'allowSignedRequest' => false, // optional, but should be set to false for non-canvas apps
+            );
+          
+            $facebook = new Facebook($fb_config);
+            
+            $out .= '<br><br><div><a name="facebook" href="#facebook" onclick="toggle_visibility(' . "'facebook'" . ');">Facebook</a></div><br>';
+            $out .= '<div id="facebook" class="event_social" style="display:none">';
+            
+            $out .= "<h2>Facebook Event:</h2>";
+ 
+            $fb_pic = $facebook->api("/" . $fb_event_id . "?fields=picture.width(99999).height(99999)");    //mit diesen Werten wird das größte verfügbare Bilde genommen, ansonsten skaliert FB die meisten Bilder runter
+            $fb_pic = $fb_pic['picture']['data']['url'];
+            $out .= '<img src="' . $fb_pic . '" alt="Facebook Picture"><br>';
+            
+            $fb_link = "https://www.facebook.com/events/" . $fb_event_id . "/";            
+            $fb_name = $facebook->api("/" . $fb_event_id . "?fields=name");
+            $fb_name = $fb_name['name'];
+            
+            $out .= '<strong><a href="' . $fb_link . '">' . $fb_name . "</a></strong><br><br>";
+            
+            $attending = $facebook->api("/" . $fb_event_id . "/attending");
+            $attending = $attending['data'];
+            
+            $out .= count($attending) . " G&auml;ste haben zugesagt!<br><br>";
+            
+            //wenn mehr als 20 Teilnehmer dabei sind, werden sie nebeneinander gelistet
+            foreach ($attending as $attendee) {
+                $out .= '<a href="https://www.facebook.com/' . $attendee['id'] . '">' . $attendee['name'] . "</a>";
+                if (count($attending) > 20) {
+                    $out .= ", ";
+                }
+                else {
+                    $out .= "<br>";
+                }
+            }
+            
+            $out .= '<br><br><br><strong>Neueste Kommentare:</strong><br><br>';
+            
+            $feed = $facebook->api("/" . $fb_event_id . "/feed");
+            $feed = $feed['data'];
+            
+            foreach ($feed as $post) {
+                if ($post['type'] == "status") {
+                    $out .= '<a href="https://www.facebook.com/' . $post['from']['id'] . '">' . $post['from']['name'] . "</a><br>";
+                    $out .= $post['message'] . "<br><br>";
+                }
+                if ($post['story']) {
+                    $out .= '<a href="https://www.facebook.com/' . $post['from']['id'] . '">' . $post['from']['name'] . "</a><br>";                   
+                    $out .= $post['story'] . "<br>";
+                    if ($post['type'] == "photo") {
+                        $out .= '<img src="' . $post['picture'] . '" alt="Facebook Photo"><br>';
+                    }
+                    $out .= "<br>";
+                }
+            }       
+            $out .= '<br><a href="#facebook">Nach oben.</a>';
+            $out .= "</div>";   //Ende des Facebook Wrappers
+        }
+        
+        
+        //Twitter Hashtag --- reference: https://dev.twitter.com/docs/api/1.1/get/search/tweets, tutorial: http://www.youtube.com/watch?v=iPnGB7a7dO0
+        if($hashtag) {
+            
+            $consumer = "1stozpaVb8gfSdd15XI6xQ";
+            $consumersecret = "9scMIXAMJdZsPrTCeFyDPB35QU4aXcnkAoQ1u38Y";
+            $accesstoken = "2279704992-8Mf75D8VWn8VlRIc3oZbMnlyvR4c045Sl22r3am";
+            $accesstokensecret = "olZqD6MOdr86yv0qE56J5Q15QEQyB1mwnhX2cXtS8qzaZ";
+
+            $out .= '<br><br><div><a name="twitter" href="#twitter" onclick="toggle_visibility(' . "'twitter'" . ');">Twitter</a></div><br>';
+            $out .= '<div id="twitter" class="event_social" style="display:none">';
+            
+            $out .= '<nr><img src="img/twitter_logo.png" alt="Twitter Bird" height="30px"><span class="hashtag">#' . $hashtag . '</span><br>';
+            
+            $twitter = new TwitterOAuth($consumer, $consumersecret, $accesstoken, $accesstokensecret);
+            $tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q=%23'.$hashtag.'&result_type=mixed');    //%23 wird als # aufgelöst (Hashtag)    //weitere Parameter: https://dev.twitter.com/docs/api/1.1/get/search/tweets
+            
+            $count = 0;
+            foreach($tweets as $tweet) {
+                foreach($tweet as $t) {
+                    $count++;
+                    if($count == 10) {
+                        break;
+                    }
+                    $out .= '<div class="tweets"><img src="'.$t->user->profile_image_url.'" />';
+                    $out .= '<span class="twittername">    ' . $t->user->name . '</span><br>';
+                    $out .= '<div class="twittertext">' . $t->text.'</div></div><br>';                                        //text gibt den Tweet aus
+                }
+                if($count == 10) {
+                    break;
+                }
+            }
+            $out .= '<br><a href="#twitter">Nach oben.</a>';
+            $out .= "</div>";   //Ende des Twitter Wrappers            
+        }
+        
+
         /*
          * flickr photo tag  // API reference: http://www.flickr.com/services/api/
          */
         if($flickrtag) {
-             
+            
+            $out .= '<br><br><div><a name="flickr" href="#flickr" onclick="toggle_visibility(' . "'flickr'" . ');">Flickr</a></div><br>';
+            $out .= '<div id="flickr" class="event_social" style="display:none">';
+                         
             $out .= "Flickr-Photos mit Tag $flickrtag:<br>";
             
             $flickrkey = "27d0025e89ef414fcc5671a3dcad6ed6";
@@ -218,7 +260,8 @@ class Eventdetailview {
                     $out .= '<div><img border="0" alt="'. $photo[title] . '" src="
                         ' . $flickrurl . '" /><br /><p>' . $photo[title] . '</p></div>';
             }
-            
+            $out .= '<br><a href="#flickr">Nach oben.</a>';
+            $out .= "</div>";   //Ende des Flickr Wrappers
         }
         
         // flickr photo album
